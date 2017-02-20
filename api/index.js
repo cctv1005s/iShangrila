fdd242a13cd8b9aa3ab9d13dd62948062bcf4306
GorgearYang <384113872@qq.com>
2017-02-20 10:51:59 +0800

    add react

--- api/index.js
+++ api/index.js
@@ -2,13 +2,25 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var EventProxy = require('eventproxy');
+var cheerio = require('cheerio');
 
 router.get('/',function(req,res,next){
 	res.render('index');
 });
 
-router.get('/share',function(req,res,next){
 
+var share_url = 'http://www.mafengwo.cn/search/s.php?q=%E9%A6%99%E6%A0%BC%E9%87%8C%E6%8B%89&t=article_gonglve';
+router.get('/share',function(req,res,next){
+	get_share(function(){
+		res.send("test");
+	});
 });
+
+var get_share = function(cb){
+	request(share_url,function(err,req,body){
+		console.log(body);
+		var $ = cheerio.load(body);
+	});
+}
 
 module.exports = router;
