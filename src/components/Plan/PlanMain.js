f31ab295bbacb93da0d6fe7e394c969d06a881db
GorgearYang <384113872@qq.com>
2017-02-23 19:02:40 +0800

    添加了百度地图

--- src/components/Plan/PlanMain.js
+++ src/components/Plan/PlanMain.js
@@ -17,22 +17,29 @@ class PlanMain extends React.Component{
     }
 
     componentDidMount(){
+        $("#map-bg").css('height',window.innerHeight - 64);
+        var map = new BMap.Map("map-bg");          // 创建地图实例  
+        var point = new BMap.Point(116.404, 39.915);  // 创建点坐标  
+        map.centerAndZoom(point, 15);                 // 初始化地图，设置中心点坐标和地图级别
     }
 
     render(){
         return (
           <MuiThemeProvider>
-            <div>
+            <div className="plan">
               <AppBar 
               　className="AppBar"
               　title="我的香格里拉"
               />
-                 <RaisedButton
+              <div className="plan-main">
+                <div id="map-bg"></div>
+                <RaisedButton
                   href="https://github.com/callemall/material-ui"
                   target="_blank"
                   label="定制个人旅行计划"
                   secondary={true}
                 />
+              </div>
               <Footer />
             </div>
           </MuiThemeProvider>
